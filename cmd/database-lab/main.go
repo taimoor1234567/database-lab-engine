6afccc0ca123b6b114fa368a53ee3d5b1fb60549
akartasov <agneum@gmail.com>
2021-11-10 09:44:37 +0700

    feat: provide the DLE container name to control container labels

--- cmd/database-lab/main.go
+++ cmd/database-lab/main.go
@@ -10,8 +10,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -69,18 +71,19 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	hostname := os.Getenv("HOSTNAME")
-	if hostname == "" {
-		log.Err("hostname is empty")
+	engProps, err := getEngineProperties(ctx, dockerCLI, cfg.Global.InstanceID)
+	if err != nil {
+		log.Err("failed to get Database Lab Engine properties:", err.Error())
+		return
 	}
 
-	internalNetworkID, err := networks.Setup(ctx, dockerCLI, cfg.Global.InstanceID, hostname)
+	internalNetworkID, err := networks.Setup(ctx, dockerCLI, cfg.Global.InstanceID, engProps.ContainerName)
 	if err != nil {
 		log.Errf(err.Error())
 		return
 	}
 
-	defer networks.Stop(dockerCLI, internalNetworkID, hostname)
+	defer networks.Stop(dockerCLI, internalNetworkID, engProps.ContainerName)
 
 	// Create a platform service to make requests to Platform.
 	platformSvc, err := platform.New(ctx, cfg.Platform)
@@ -110,7 +113,7 @@ func main() {
 	}
 
 	// Create a new retrieval service to prepare a data directory and start snapshotting.
-	retrievalSvc := retrieval.New(cfg, dockerCLI, pm, tm, runner)
+	retrievalSvc := retrieval.New(cfg, engProps, dockerCLI, pm, tm, runner)
 
 	if err := retrievalSvc.Run(ctx); err != nil {
 		log.Err("Failed to run the data retrieval service:", err)
@@ -149,7 +152,7 @@ func main() {
 		Restore:       retrievalSvc.CollectRestoreTelemetry(),
 	})
 
-	server := srv.NewServer(&cfg.Server, &cfg.Global, cloningSvc, retrievalSvc, platformSvc, dockerCLI, obs, est, pm, tm)
+	server := srv.NewServer(&cfg.Server, &cfg.Global, engProps, cloningSvc, retrievalSvc, platformSvc, dockerCLI, obs, est, pm, tm)
 	shutdownCh := setShutdownListener()
 
 	go setReloadListener(ctx, provisionSvc, tm, retrievalSvc, pm, cloningSvc, platformSvc, est, server)
@@ -176,6 +179,25 @@ func main() {
 	shutdownDatabaseLabEngine(shutdownCtx, dockerCLI, cfg.Global, pm.First().Pool())
 	cloningSvc.SaveClonesState()
 	tm.SendEvent(ctx, telemetry.EngineStoppedEvent, telemetry.EngineStopped{Uptime: server.Uptime()})
+}
+
+func getEngineProperties(ctx context.Context, dockerCLI *client.Client, instanceID string) (global.EngineProps, error) {
+	hostname := os.Getenv("HOSTNAME")
+	if hostname == "" {
+		return global.EngineProps{}, errors.New("hostname is empty")
+	}
+
+	dleContainer, err := dockerCLI.ContainerInspect(ctx, hostname)
+	if err != nil {
+		return global.EngineProps{}, fmt.Errorf("failed to inspect DLE container: %w", err)
+	}
+
+	engProps := global.EngineProps{
+		InstanceID:    instanceID,
+		ContainerName: strings.Trim(dleContainer.Name, "/"),
+	}
+
+	return engProps, nil
 }
 
 func reloadConfig(ctx context.Context, provisionSvc *provision.Provisioner, tm *telemetry.Agent, retrievalSvc *retrieval.Retrieval,
