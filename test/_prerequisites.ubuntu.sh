18d2d8036b0b6e35d73cb36e2ed34fdb939c0f3f
akartasov <agneum@gmail.com>
2021-11-17 19:38:07 +0700

    chore: refactor tests configuration, get rid of sed-instructions

--- test/_prerequisites.ubuntu.sh
+++ test/_prerequisites.ubuntu.sh
@@ -29,3 +29,8 @@ sudo apt-get install -y \
 # Install psql
 sudo apt-get install -y \
   postgresql-client
+
+# Install yq
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
+sudo add-apt-repository ppa:rmescandon/yq
+sudo apt update && sudo apt install yq -y
