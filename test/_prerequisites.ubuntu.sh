5194f1bc0daa5fec6794ec5d10997d56adc904c1
akartasov <agneum@gmail.com>
2021-11-19 19:04:56 +0700

    run without sudo

--- test/_prerequisites.ubuntu.sh
+++ test/_prerequisites.ubuntu.sh
@@ -2,12 +2,13 @@
 set -euxo pipefail
 
 # Install dependencies
-sudo apt-get update && sudo apt-get install -y \
+apt-get update && apt-get install -y \
   apt-transport-https \
   ca-certificates \
   curl \
   gnupg-agent \
-  software-properties-common
+  software-properties-common \
+  sudo
 
 # Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
