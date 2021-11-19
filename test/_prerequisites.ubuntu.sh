aa600233c817592a90e5c99554d1f75323ec517e
akartasov <agneum@gmail.com>
2021-11-19 19:26:05 +0700

    install yq, enable logical test

--- test/_prerequisites.ubuntu.sh
+++ test/_prerequisites.ubuntu.sh
@@ -10,20 +10,18 @@ apt-get update && apt-get install -y \
   software-properties-common \
   sudo
 
-lsb_release -a
-
 # Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-#sudo add-apt-repository \
-#  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) \
-#  stable"
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
 
-# sudo apt-get install -y \
-#  docker-ce \
-#  docker-ce-cli \
-#  containerd.io
+sudo apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io
 
 # Install ZFS
 sudo apt-get install -y \
@@ -35,9 +33,7 @@ sudo apt-get install -y \
 
 # Install yq
 if ! command -v yq &> /dev/null; then
-  sudo curl -L https://github.com/mikefarah/yq/releases/download/v4.14.2/yq_linux_amd64 -o /usr/bin/yq && sudo chmod +x /usr/bin/yq
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64 && \
+  sudo add-apt-repository ppa:rmescandon/yq && \
+  sudo apt-get update && sudo apt-get install yq -y
 fi
-# TODO: Uncomment after upgrade the runner machine
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
-# sudo add-apt-repository ppa:rmescandon/yq
-# sudo apt-get update && sudo apt-get install yq -y
