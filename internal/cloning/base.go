03f8457d165462aec56f69faff5871c5f4a8cca9
akartasov <agneum@gmail.com>
2021-12-01 20:04:14 +0700

    feat: immediate start HTTP server in DLE and provide the actual instance status (#307)

--- internal/cloning/base.go
+++ internal/cloning/base.go
@@ -300,7 +300,10 @@ func (c *Base) DestroyClone(cloneID string) error {
 
 	if w.Session == nil {
 		c.deleteClone(cloneID)
-		c.decrementCloneNumber(w.Clone.Snapshot.ID)
+
+		if w.Clone.Snapshot != nil {
+			c.decrementCloneNumber(w.Clone.Snapshot.ID)
+		}
 
 		return nil
 	}
@@ -470,18 +473,16 @@ func (c *Base) ResetClone(cloneID string, resetOptions types.ResetCloneRequest) 
 	return nil
 }
 
-// GetInstanceState returns the current state of instance.
-func (c *Base) GetInstanceState() (*models.InstanceStatus, error) {
+// GetCloningState returns the current state of instance.
+func (c *Base) GetCloningState() models.Cloning {
 	clones := c.GetClones()
-	c.instanceStatus.Cloning = models.Cloning{
+	cloning := models.Cloning{
 		ExpectedCloningTime: c.getExpectedCloningTime(),
 		Clones:              clones,
 		NumClones:           uint64(len(clones)),
 	}
-	c.instanceStatus.Pools = c.provision.GetPoolEntryList()
-	c.instanceStatus.Engine.Telemetry = pointer.ToBool(c.tm.IsEnabled())
 
-	return c.instanceStatus, nil
+	return cloning
 }
 
 // GetSnapshots returns all available snapshots.
